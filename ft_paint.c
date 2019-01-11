ed138630d528de8048be6c2d43ab35faee2f887b
Melodie Denis <medenis@w-r9-p7.s19.be>
2019-01-11 15:37:47 +0100

    11 01 2019 1537

--- ft_paint.c
+++ ft_paint.c
@@ -6,7 +6,7 @@
 /*   By: medenis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 19:49:05 by medenis           #+#    #+#             */
-/*   Updated: 2019/01/11 15:15:24 by medenis          ###   ########.fr       */
+/*   Updated: 2019/01/11 15:36:22 by medenis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int		ft_paint(char **str)
 	}
 	while (i < 16)
 	{
+		if (str[i / 4][i % 4] != "#" && str[i / 4][i % 4] != ".")
+			return (-1);
 		if (str[i / 4][i % 4] == '#')
 		{
 			str[i / 4][i % 4] = c;
@@ -35,7 +37,7 @@ int		ft_paint(char **str)
 		i++;
 	}
 	c++;
-	if (count != 4)
+	if (count != 4 || i != 16)
 		return (-1);
 	return (1);
 }
